4d180101e4735788a2245164f84d50ff3cbf84cb
kowaliczek_256 <kowalik.piotr256@gmail.com>
2025-09-14 21:03:35 +0200

    update main with basic behavior

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QPushButton, QWidget
+from PySide6.QtGui import QAction
 
 
 class MainWindow(QMainWindow):
@@ -9,10 +10,23 @@ class MainWindow(QMainWindow):
     """
 
     def __init__(self):
+        """
+        Constructor. Initialize a window.
+        """
         super().__init__()
         loader = QUiLoader()
         main_window_path_file = Path(__file__).parent.parent / "ui/main_window.ui"
         self.main_window = loader.load(main_window_path_file)
 
+        self.action_close_app = self.main_window.findChild(QAction, "actionClose")
+        self.action_close_app.triggered.connect(self.close_app)
+
     def show(self):
+        """
+        Show the main window widget.
+        :return:
+        """
         self.main_window.show()
+
+    def close_app(self):
+        self.main_window.close()
